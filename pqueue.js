a92af232f8a7e7b1bc69a2ec5b6f1ed0bb6abeda
Vladimir Kargov <kargov@gmail.com>
2018-08-23 22:58:33 +0300

    [queue] finish heapsort implementation

--- pqueue.js
+++ pqueue.js
@@ -1,25 +1,34 @@
 // Heap implementation heavily inspired by the one described in
 // Introduction to Algorithms
 
-function MaxHeapify(A, i) {
-	i_max = i
-	left = i*2+1
-	right = i*2+2
-	if (left < A.length && A[left] > A[i_max])
+function MaxHeapify(A, i, len) {
+	if (len === undefined)
+		len = A.length
+	var i_max = i
+	var left = i*2+1
+	var right = i*2+2
+	if (left < len && A[left] > A[i_max])
 		i_max = left
-	if (right < A.length && A[right] > A[i_max])
+	if (right < len && A[right] > A[i_max])
 		i_max = right
-	if (i_max === i)
-		return
-	tmp = A[i]
-	A[i] = A[i_max]
-	A[i_max] = tmp
-	MaxHeapify(A, i_max)
+	if (i_max !== i) {
+		[A[i], A[i_max]] = [A[i_max], A[i]]
+		MaxHeapify(A, i_max, len)
+    }
 }
 
 function BuildMaxHeap(A) {
 	for (var i = Math.floor(A.length / 2); i >= 0; i--)
 		MaxHeapify(A, i)
+}
+
+function Heapsort(A) {
+	BuildMaxHeap(A)
+	for (var i = A.length - 1; i > 0; i--) {
+		debugger
+		[A[0], A[i]] = [A[i], A[0]]
+		MaxHeapify(A, 0, i)
+    }
 }
 
 class PriorityQueue {
